ec41ab514f11254339d3ce1267a22abc85ab380a
Alex93Bakun <56162141+Alex93Bakun@users.noreply.github.com>
2021-11-04 12:27:34 +0200

    Liking System, refactoring, styles

---
+++ routes/Likes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { Likes } = require('../models');
+const { validateToken } = require('../middlewares/authMiddleware');
+
+router.post('/', validateToken, async (req, res) => {
+    const { PostId } = req.body;
+    const UserId = req.user.id;
+
+    const found = await Likes.findOne({ where: { PostId, UserId } });
+    if (found) {
+        await Likes.destroy({ where: { PostId, UserId } });
+        res.json({ liked: false });
+    } else {
+        await Likes.create({ PostId, UserId });
+        res.json({ liked: true });
+    }
+});
+
+module.exports = router;
